3f2058d28b96d8c18c5ffbf30e628a96b8732755
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-03-09 16:59:36 +0100

    MAINT: Moved webpack to stack repository since it needs to collect JS from plugins

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,53 @@ const path = require('path');
 const {VueLoaderPlugin} = require('vue-loader');
 
 module.exports = {
-  entry: {
-    base: './topobank/frontend/manager/base.js',
-    select: './topobank/frontend/manager/select.js',
-    dzi: './topobank/frontend/manager/dzi.js',
-    series_card: './topobank/frontend/analysis/series_card.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'topobank/static/js'),
-    filename: '[name].bundle.js',
-    library: ['topobank', '[name]']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
+    entry: {
+        base: 'topobank/manager/base.js',
+        select: 'topobank/manager/select.js',
+        dzi: 'topobank/manager/dzi.js',
+        series_card: 'topobank/analysis/series_card.js',
+        roughness_parameters_card: 'topobank_statistics/analysis/roughness_parameters_card.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'topobank/topobank/static/js'),
+        filename: '[name].bundle.js',
+        library: ['topobank', '[name]']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
         ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
+    },
+    resolve: {
+        alias: {
+            topobank: path.resolve(__dirname, 'topobank/topobank/frontend'),
+            topobank_statistics: path.resolve(__dirname, 'topobank-statistics/topobank_statistics/frontend'),
+            topobank_contact: path.resolve(__dirname, 'topobank-contact/topobank_contact/frontend')
+        },
+        extensions: [
+            '.js',
+            '.vue'
         ]
-      }
+    },
+    plugins: [
+        new VueLoaderPlugin()
     ]
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.vue'
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
 };
