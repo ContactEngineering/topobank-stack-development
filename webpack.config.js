a428a71ee0e15d9a989a961a7b4f1e411197f973
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-03-17 11:35:48 +0100

    TypeScript webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,40 @@ const {VueLoaderPlugin} = require('vue-loader');
 
 module.exports = {
     entry: {
-        app: 'topobank/app.js'
-    },
-    output: {
+        app: 'topobank/app.ts'
+    }, output: {
         path: path.resolve(__dirname, 'static/js'),
         filename: '[name].bundle.js',
         library: ['topobank', '[name]']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ],
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    resolve: {
+    }, module: {
+        rules: [{
+            test: /\.ts$/, use: [{
+                loader: "ts-loader",
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                    transpileOnly: true,
+                },
+            }], exclude: /node_modules/
+        }, {
+            test: /\.vue$/, loader: 'vue-loader', options: {
+                loaders: {
+                    js: 'babel-loader',
+                }
+            }, exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            use: ['vue-style-loader', 'css-loader'],
+            exclude: /node_modules/,
+        }, {
+            test: /\.s[ac]ss$/,
+            use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+            exclude: /node_modules/,
+        }]
+    }, resolve: {
         alias: {
             topobank: path.resolve(__dirname, 'ce-ui/frontend'),
             topobank_statistics: path.resolve(__dirname, 'topobank-statistics/frontend'),
             topobank_contact: path.resolve(__dirname, 'topobank-contact/frontend')
-        },
-        extensions: [
-            '.js',
-            '.ts',
-            '.scss',
-            '.vue'
-        ]
-    },
-    plugins: [
-        new VueLoaderPlugin()
-    ]
+        }, extensions: ['.js', '.ts', '.scss', '.vue']
+    }, plugins: [new VueLoaderPlugin()]
 };
